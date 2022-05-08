d9c09f32d2763c5d26153a7ee0e8bdb8e422781a
kristoffervelazquez <kristoffervelazquez49@live.com>
2022-05-08 04:56:01 -0700

    Se agrega la conexión a la base de datos

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import express from 'express';
 import router from './routes/index.js';
+import db from './config/db.js'
 
 const app = express();
+
+// Conectar a la base de datos
+db.authenticate()
+    .then(() => console.log('Base de datos conectada con éxito'))
+    .catch( err => console.error(err))
+
+
 
 // Definir puerto
 const port = process.env.PORT || 3000;
@@ -33,5 +41,5 @@ app.use(express.static('public'));
 
 
 app.listen(port, () => {
-    console.log(`El servidor está funcionando en el puerto ${port}`);
+    console.log(`El servidor está funcionando en el puerto ${port} http://localhost:3000/`);
 })
