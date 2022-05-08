b6eaa55b83401f29b953c45b49b4305b31115462
kristoffervelazquez <kristoffervelazquez49@live.com>
2022-05-08 02:27:24 -0700

    Se agregaron todas las vistas

--- index.js
+++ index.js
@@ -6,19 +6,31 @@ const app = express();
 // Definir puerto
 const port = process.env.PORT || 3000;
 
+// Obtener el año actual
+app.use( (req, res, next) =>{
+
+    const year= new Date();
+        
+    res.locals.actualYear = year.getFullYear(); 
+    console.log(res.locals)
+    next();
+})
+
+
 // Agregar router
-app.use('/', router);
+app.use('/', router); 
 
 // Habilitar PUG
 app.set('view engine', 'pug');
 
 
 
-
+// Definir carpeta publica
+app.use(express.static('public'));
 
 
 
 
 app.listen(port, () => {
-    console.log(`El servidor está funcionando en el puerto ${port}`)
+    console.log(`El servidor está funcionando en el puerto ${port}`);
 })
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,25 @@ import express from "express"
 const router = express.Router();
 
 router.get('/', (req, res) =>{ // req - lo que enviamos : res - lo que express responde
-    res.render('inicio')
+    res.render('inicio', {
+        pagina: 'Inicio'
+    } )
 });
-
 router.get('/nosotros', (req, res) =>{ // req - lo que enviamos : res - lo que express responde
-    
-    res.render('nosotros')
+    res.render('nosotros', {
+        pagina: 'Nosotros'
+    } )
 });
 
+router.get('/viajes', (req, res) =>{ // req - lo que enviamos : res - lo que express responde
+    res.render('viajes', {
+        pagina: 'Viajes'
+    } )
+});
+
+router.get('/testimoniales', (req, res) =>{ // req - lo que enviamos : res - lo que express responde
+    res.render('testimoniales', {
+        pagina: 'Testimoniales'
+    } )
+});
 export default router;
