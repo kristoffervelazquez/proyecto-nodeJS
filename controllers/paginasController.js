1764a3f48e76b27cf7da2453d6b210a0f5ed40dc
kristoffervelazquez <kristoffervelazquez49@live.com>
2022-05-09 16:32:43 -0700

    Se finaliza el proyecto y se mejora el rendimiento con Promises

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -2,10 +2,26 @@ import { Viaje } from '../models/Viaje.js';
 import { Testimonial } from '../models/Testimoniales.js'
 
 
-const paginaInicio = (req, res) => {
-    res.render('inicio', {
-        pagina: 'Inicio'
-    })
+const paginaInicio = async (req, res) => {
+
+    // Consultar 3 viajes del modelo viaje y 3 testimoniales
+    const promiseDB = []; 
+    promiseDB.push(Viaje.findAll({limit: 3}))
+    promiseDB.push(Testimonial.findAll({limit: 3}))
+
+    try {
+        // Se crea el promise que tiene los multiples awaits de entidades diferentes
+        const result = await Promise.all(promiseDB);
+        res.render('inicio', {
+            pagina: 'Inicio',
+            clase: 'home',
+            viajes: result[0],
+            testimoniales: result[1]
+        })
+    } catch (error) {
+        console.error(error)
+    }
+
 }
 
 const paginaNosotros = (req, res) => { // req - lo que enviamos : res - lo que express responde
