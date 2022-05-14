51223870d538158330f5b5afe71473339665d13c
kristoffervelazquez <kristoffervelazquez49@live.com>
2022-05-13 17:23:12 -0700

    Se modifican variables de entorno

--- config/db.js
+++ config/db.js
@@ -1,11 +1,9 @@
 import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
 
-dotenv.config({path:"variables.env"});
+dotenv.config({ path: "variables.env" });
 
-const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS, {
-    host: process.env.BD_HOST,
-    port: process.env.BF_PORT,
+const db = new Sequelize(process.env.BD_URL, {
     dialect: 'mysql',
     define: {
         timestamps: false
@@ -15,8 +13,28 @@ const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env
         min: 0,
         acquire: 30000,
         idle: 10000
-    }, 
+    },
     operatorAliases: false
-});
+})
+
+
+// const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS, {
+//     host: process.env.BD_HOST,
+//     port: process.env.BF_PORT,
+//     dialect: 'mysql',
+//     define: {
+//         timestamps: false
+//     },
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }, 
+//     operatorAliases: false
+// });
+
+
+
 
 export default db;
